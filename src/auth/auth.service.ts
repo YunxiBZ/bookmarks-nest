4d96f38cd1403102ffd9767f638f6646db8b84ac
yunxi <zhangyunxi2012@gmail.com>
2022-05-19 17:15:20 +0200

    Init prisma, add prisma feature for gestion db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   signup() {
     // return response, nest will handle the response type (text or json etc)
     return { message: 'I am signed up' };
