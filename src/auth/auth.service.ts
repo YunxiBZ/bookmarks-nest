3202430cdf40c92fbedee15c52a4844381aae8f8
yunxi <zhangyunxi2012@gmail.com>
2022-05-25 14:50:11 +0200

    Handle signup with Unique constraint for email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    // generate the password hash
-    const hash = await argon.hash(dto.password);
-    // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // delete the hash before return user info
-    delete user.hash;
-    // return the saved user
-    // return response, nest will handle the response type (text or json etc)
-    return user;
+      // delete the hash before return user info
+      delete user.hash;
+      // return the saved user
+      // return response, nest will handle the response type (text or json etc)
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        //  'P2002' is a Prisma Error Code : "Unique constraint failed on the {constraint}"
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
